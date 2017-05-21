70d3507dd7b9924367721ba7a9ea4786dd98a176
Abhimanyu Arora <er.abhimanyuarora@gmail.com>
2017-05-21 16:06:15 +0530

    Added domain name in the config

--- build/config.js
+++ build/config.js
@@ -4,6 +4,7 @@ const pkg = require('../package')
 module.exports = {
   port: 4000,
   title: 'ipl-dashboard',
+  domain: 'ipl-dashboard.surge.sh',
   // when you use electron please set to relative path like ./
   // otherwise only set to absolute path when you're using history mode
   publicPath: '/',
