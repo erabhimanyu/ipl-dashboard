0151627c7cc4ad7a8b81fb1593b800aad67a03c0
Abhimanyu Arora <er.abhimanyuarora@gmail.com>
2017-05-22 17:42:40 +0530

    Refactored home page into smaller components

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="page">
-    <counter></counter>
-    <p>
-      So now I will start IPL Dashboard work on this
-    </p>
+    <div class="home-wrapper">
+    	<ForkGithub></ForkGithub>
+    	<Header></Header>
+    	<img src="/images/stadium.png">
+    	<div class="footer"></div>
+    </div>
   </div>
 </template>
 
 <script>
 import Counter from 'components/Counter'
+import Header from 'components/Header'
+import ForkGithub from 'components/ForkGithub'
 
 export default {
   components: {
-    Counter
+    Counter,
+    Header,
+    ForkGithub
   }
 }
 </script>
+<style>
+	.home-wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+	}
+</style>
