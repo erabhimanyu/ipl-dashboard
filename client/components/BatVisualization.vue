a887cc6d6283e2529fe635dcdad0a566bf964e7f
Abhimanyu Arora <er.abhimanyuarora@gmail.com>
2017-05-22 21:32:41 +0530

    Added batVisualization component

---
+++ client/components/BatVisualization.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="bat-visualization-wrapper">
+    <img src="/images/batsman.png">
+   </div>
+</template>
+<style>
+	.bat-visualization-wrapper {
+		background-image: url('/images/patch.png');
+		background-size: contain;
+		background-repeat: no-repeat;
+		background-position: center;
+		min-width: 100%;
+		min-height: 500px;
+		display: flex;
+		flex-direction: column;
+    align-items: flex-start;
+    	
+    	img {
+    		zoom: 0.7;
+    		padding: 200px;
+    	}
+	}
+</style>
