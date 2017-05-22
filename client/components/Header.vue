b455d2b34794f53a675d3d391f44e0a93ede3457
Abhimanyu Arora <er.abhimanyuarora@gmail.com>
2017-05-22 17:41:58 +0530

    Added header component

---
+++ client/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="header">
+    			<div class="menu">
+    				<span>Home</span>
+    				<span>Whose Paradise?</span>
+    				<span>Team</span>
+    				<span>About</span>
+    			</div>
+    	</div>
+</template>
+
+
+<style>
+	.header {
+		display: flex;
+	}
+	.header .menu {
+		display: flex;
+		flex-direction: row;
+		align-items: flex-end;
+		margin-top: 20px;
+		span {
+			padding: 20px;
+			font-family: MarkerFelt;
+			margin-right: 100px;
+			color: #616161;
+		}
+	}
+</style>
