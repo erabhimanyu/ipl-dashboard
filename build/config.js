4974a2bd75019f0be036a9c698ff8b49efe3cc3e
Abhimanyu Arora <er.abhimanyuarora@gmail.com>
2017-05-22 13:57:06 +0530

    Title updated

--- build/config.js
+++ build/config.js
@@ -3,7 +3,7 @@ const pkg = require('../package')
 
 module.exports = {
   port: 3000,
-  title: 'ipl-dashboard',
+  title: 'IPL-Dashboard',
   domain: 'ipl-dashboard.surge.sh',
   // when you use electron please set to relative path like ./
   // otherwise only set to absolute path when you're using history mode
