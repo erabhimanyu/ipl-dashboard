b970944bfa17c6338c3cca666f4018a258721617
Abhimanyu Arora <er.abhimanyuarora@gmail.com>
2017-05-22 17:41:38 +0530

    Added fork-me-github image and component

---
+++ client/components/ForkGithub.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="fork-github">
+    <a href="https://github.com/erabhimanyu/ipl-dashboard" target="_blank">
+    	<img src="/images/forkmeGithub.png" alt="Fork me on GitHub">
+    </a>
+   </div>
+</template>
+
+<style>
+	.fork-github {
+		width: 100%;
+		img {
+			position: fixed;
+			right: 0px;
+			top: 0px;
+			width: 15%;
+		}
+	}
+</style>
