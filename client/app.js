7844eec523a496ae961d495332a052698eeef91a
Abhimanyu Arora <er.abhimanyuarora@gmail.com>
2017-05-22 17:40:33 +0530

    Added common css and imported fontawesome

--- client/app.js
+++ client/app.js
@@ -3,7 +3,8 @@ import { sync } from 'vuex-router-sync'
 import App from './components/App'
 import router from './router'
 import store from './store'
-import 'font-awesome/css/font-awesome.css';
+import 'font-awesome/css/font-awesome.css'
+import 'common.css'
 
 const app = new Vue({
   router,
