42b86b89e479f3a7eab8e8512d63c837b115d18b
Abhimanyu Arora <er.abhimanyuarora@gmail.com>
2017-05-25 01:27:13 +0530

    Team page added

---
+++ client/views/TeamPage.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="main-wrapper">
+		<ForkGithub></ForkGithub>
+	  <HeaderElement></HeaderElement>
+	  <div class="content-wrapper">
+	  	<p>{{ data.fullName }}</p>
+	  	<div class="team-info-wrapper">
+	  		<span>
+	  			<img v-bind:src="imgLink">
+	  		</span>
+	  	</div>
+	  </div>
+	  <FooterElement></FooterElement>
+	</div>
+</template>
+<script>
+import HeaderElement from 'components/Header'
+import ForkGithub from 'components/ForkGithub'
+import FooterElement from 'components/Footer'
+import {teams} from '../Config.js'
+
+export default {
+  components: {
+    HeaderElement,
+    ForkGithub,
+    FooterElement
+  },
+  data: function() {
+  	return {data: teams[this.$route.params.teamName]}
+  },
+  computed: {
+  	imgLink: function() {
+  		return this.data.imageSrc
+  	},
+  	teamName: function() {
+  		return this.data.name
+  	}
+  }
+}
+</script>
+
+<style>
+	.team-info-wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		margin-left: 300px;
+		span {
+			width: 160px;
+			height: 110px;
+			background-color: rgba(128, 128, 128, 0.28);
+			text-align: center;
+			padding-top: 10px; 
+		}
+		img {
+			max-width: 150px;
+			height: 100px;
+		}
+	}
+</style>
