4f602162fc7542185115c0fa3351ffed7c7523f1
Abhimanyu Arora <er.abhimanyuarora@gmail.com>
2017-05-25 01:30:06 +0530

    Data extracted into a config file

---
+++ client/Config.js
@@ -0,0 +1,72 @@
+
+let baseImagePath = '/images/teams'
+
+module.exports = {
+  teams: {
+    KKR: {
+      imageSrc: baseImagePath+'/KKR.jpg',
+      fullName: 'Kolkata Knight Riders',
+      wiki_link: ''
+    },
+    CSK: {
+      imageSrc: baseImagePath+'/CSK.png',
+      fullName: 'Chennai Super Kings',
+      wiki_link: ''
+    },
+    DC: {
+      imageSrc: baseImagePath+'/DC.png',
+      fullName: 'Deccan Chargers',
+      wiki_link: ''
+    },
+    DDD: {
+      imageSrc: baseImagePath+ '/DDD.png',
+      fullName: 'Delhi Dare Devil',
+      wiki_link: ''
+    },
+    GL: {
+      imageSrc: baseImagePath+ '/GL.png',
+      fullName: 'Gujarat Lions',
+      wiki_link: ''
+    },
+    HSR: {
+      imageSrc: baseImagePath+ '/HSR.png',
+      fullName: 'Hyderabad Sunrisers',
+      wiki_link: ''
+    },
+    KEP: {
+      imageSrc:baseImagePath+ '/KEP.png',
+      fullName: 'Kings IX Punjab',
+      wiki_link: ''
+    },
+    KT: {
+      imageSrc: baseImagePath+ '/KT.png',
+      fullName: 'Kochi Tuskers',
+      wiki_link: ''
+    },
+    MI: {
+      imageSrc: baseImagePath+ '/MI.jpg',
+      fullName: 'Mumbai Indians',
+      wiki_link: ''
+    },
+    PWI: {
+      imageSrc:baseImagePath+ '/PWI.png',
+      fullName: 'Pune Warriors India',
+      wiki_link: ''
+    },
+    RC: {
+      imageSrc:baseImagePath+ '/RC.png',
+      fullName: 'Royal Challengers',
+      wiki_link: ''
+    },
+    RP: {
+      imageSrc:baseImagePath+ '/RP.png',
+      fullName: 'Rising Pune Supergiant',
+      wiki_link: ''
+    },
+    RR: {
+      imageSrc:baseImagePath+ '/RR.png',
+      fullName: 'Rajasthan Royal',
+      wiki_link: ''
+    }
+  }
+}
