43bae22fa4b2f1f9de95566da4c2b20a3bf4ec10
Abhimanyu Arora <er.abhimanyuarora@gmail.com>
2017-05-25 01:28:13 +0530

    Basic structure added

--- client/views/About.vue
+++ client/views/About.vue
@@ -1,3 +1,23 @@
 <template>
-	<p>I am about page</p>
+	<div class="main-wrapper">
+		<ForkGithub></ForkGithub>
+	  <HeaderElement></HeaderElement>
+	  <div class="content-wrapper">
+	  <!-- Page content Goes here -->
+	  </div>
+	  <FooterElement></FooterElement>
+	</div>
 </template>
+<script>
+import HeaderElement from 'components/Header'
+import ForkGithub from 'components/ForkGithub'
+import FooterElement from 'components/Footer'
+
+export default {
+  components: {
+    HeaderElement,
+    ForkGithub,
+    FooterElement
+  }
+}
+</script>
--- client/views/WhoseParadise.vue
+++ client/views/WhoseParadise.vue
@@ -1,3 +1,23 @@
 <template>
-	<p>I am whose paradise page</p>
+	<div class="main-wrapper">
+		<ForkGithub></ForkGithub>
+	  <HeaderElement></HeaderElement>
+	  <div class="content-wrapper">
+	  <!-- Page content goes here -->
+	  </div>
+	  <FooterElement></FooterElement>
+	</div>
 </template>
+<script>
+import HeaderElement from 'components/Header'
+import ForkGithub from 'components/ForkGithub'
+import FooterElement from 'components/Footer'
+
+export default {
+  components: {
+    HeaderElement,
+    ForkGithub,
+    FooterElement
+  }
+}
+</script>
