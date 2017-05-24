4a7f8d1473e334ae059ecb528a84254019e81b19
Abhimanyu Arora <er.abhimanyuarora@gmail.com>
2017-05-25 01:28:42 +0530

    Team listing component added

--- client/views/Teams.vue
+++ client/views/Teams.vue
@@ -1,3 +1,26 @@
 <template>
-	<p>I am teams page</p>
+	<div class="main-wrapper">
+	<ForkGithub></ForkGithub>
+	  <HeaderElement></HeaderElement>
+	  <div class="content-wrapper">
+	  	<p>Disover Teams</p>
+	  	<TeamListing></TeamListing>
+	  </div>
+	  <FooterElement></FooterElement>
+	</div>
 </template>
+<script>
+import HeaderElement from 'components/Header'
+import ForkGithub from 'components/ForkGithub'
+import TeamListing from 'components/TeamListing'
+import FooterElement from 'components/Footer'
+
+export default {
+  components: {
+    HeaderElement,
+    ForkGithub,
+    TeamListing,
+    FooterElement
+  }
+}
+</script>
