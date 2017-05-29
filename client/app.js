4b77e070da9210b18d0c5d4df4b58e4b9e2e680e
Abhimanyu Arora <er.abhimanyuarora@gmail.com>
2017-05-29 05:35:58 +0530

    Unwanted packages removed

--- client/app.js
+++ client/app.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
-import { sync } from 'vuex-router-sync'
 import App from './components/App'
 import router from './router'
-import store from './store'
 import 'font-awesome/css/font-awesome.css'
 import 'common.css'
 
 const app = new Vue({
   router,
-  store,
   ...App
 })
 
-export { app, router, store }
+export { app, router }
