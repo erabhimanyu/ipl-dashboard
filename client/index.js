a9630e18d9d5d1d79e3ab285807414f1585e6dbe
Abhimanyu Arora <er.abhimanyuarora@gmail.com>
2017-05-29 12:42:21 +0530

    Offline plugin added to main entry

--- client/index.js
+++ client/index.js
@@ -1,4 +1,9 @@
 import './promise-polyfill'
 import { app } from './app'
 
+// Enable progressive web app support (with offlineplugin)
+ if (process.env.NODE_ENV === 'production') {
+   require('./pwa')
+ }
+
 app.$mount('#myApp')
